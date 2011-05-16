7b5d2941a248c93b71ace71cfd28001eb4824d81
Benjamin Saunders <besaunde@sfu.ca>
2011-05-15 19:43:34 -0700

    Switched to Mt. Gox websocket stream and added ticker data.

--- src/bitvis.c
+++ src/bitvis.c
@@ -9,22 +9,54 @@
 
 #include "net.h"
 
+#define WEBSOCKET_HOST "websocket.mtgox.com"
+
+#define WEBSOCKET_HELLO                         \
+  "GET /mtgox HTTP/1.1\r\n"                     \
+  "Upgrade: WebSocket\r\n"                      \
+  "Connection: Upgrade\r\n"                     \
+  "Host: " WEBSOCKET_HOST "\r\n"                \
+  "Origin: null\r\n"                            \
+  "\r\n"
+
+#define WEBSOCKET_REPLY                                     \
+  "HTTP/1.1 101 Web Socket Protocol Handshake\r\n"          \
+  "Upgrade: WebSocket\r\n"                                  \
+  "Connection: Upgrade\r\n"                                 \
+  "WebSocket-Origin: null\r\n"                              \
+  "WebSocket-Location: ws://websocket.mtgox.com/mtgox\r\n"  \
+  "\r\n"
+
+
 enum {
   COL_TIME,
   COL_PRICE,
   COL_VOL,
-  COL_CURR,
-  COL_SYM,
   COL_COUNT
 };
 
+typedef enum {
+  CHAN_TICKER,
+  CHAN_TRADE,
+  CHAN_DEPTH,
+  CHAN_ACCT
+} channel;
+
+#define TRADE_CHANNEL  "dbf1dee9-4f2e-4a08-8cb7-748919a71b21"
+#define TICKER_CHANNEL "d5f06780-30a8-4a48-a2f8-7ed181b4a13f"
+#define DEPTH_CHANNEL  "24e67e0d-1cad-4cc0-9e7a-f8523ef460fe"
+
 GtkListStore *trade_store;
-GIOChannel *ticker = NULL;
+GIOChannel *stream = NULL;
 GtkStatusbar *statusbar;
 struct addrinfo *servinfo;
 gchar *readbuf;
 size_t readbuf_end = 0;
 size_t readbuf_size = 1024;
+
+struct {
+  GtkLabel *high, *low, *buy, *sell, *volume;
+} ticker;
 
 void on_view_change(GtkWidget *widget, GtkAllocation *allocation, gpointer user_data) {
   GtkTreeView *view = GTK_TREE_VIEW(widget);
@@ -97,10 +129,18 @@ GtkWindow *load_ui() {
   GtkTreeView *view = GTK_TREE_VIEW(gtk_builder_get_object(builder, "treeview1"));
   statusbar = GTK_STATUSBAR(gtk_builder_get_object(builder, "statusbar"));
 
+  /* Bind ticker */
+  ticker.buy = GTK_LABEL(gtk_builder_get_object(builder, "tickbuy"));
+  ticker.sell = GTK_LABEL(gtk_builder_get_object(builder, "ticksell"));
+  ticker.low = GTK_LABEL(gtk_builder_get_object(builder, "ticklow"));
+  ticker.high = GTK_LABEL(gtk_builder_get_object(builder, "tickhigh"));
+  ticker.volume = GTK_LABEL(gtk_builder_get_object(builder, "tickvolume"));
+
+  /* Done obtaining handles */
   g_object_unref(builder);
 
   /* Configure tree view */
-  trade_store = gtk_list_store_new(COL_COUNT, G_TYPE_INT64, G_TYPE_FLOAT, G_TYPE_FLOAT, G_TYPE_STRING, G_TYPE_STRING);
+  trade_store = gtk_list_store_new(COL_COUNT, G_TYPE_INT64, G_TYPE_FLOAT, G_TYPE_FLOAT);
   g_signal_connect(view, "size-allocate", G_CALLBACK(on_view_change), NULL);
   gtk_tree_view_set_model(view, GTK_TREE_MODEL(trade_store));
 
@@ -127,27 +167,14 @@ GtkWindow *load_ui() {
     "text", COL_VOL,
     NULL);
   gtk_tree_view_append_column(view, column);
-  /* Reset renderer (default alignment) */
-  r = gtk_cell_renderer_text_new();
-  column = gtk_tree_view_column_new_with_attributes(
-    "Currency",
-    r,
-    "text", COL_CURR,
-    NULL);
-  gtk_tree_view_append_column(view, column);
-  column = gtk_tree_view_column_new_with_attributes(
-    "Symbol",
-    r,
-    "text", COL_SYM,
-    NULL);
-  gtk_tree_view_append_column(view, column);
+  gtk_tree_view_append_column(view, gtk_tree_view_column_new());
 
   g_signal_connect(root, "destroy", G_CALLBACK(gtk_main_quit), NULL);
   
   return root;
 }
 
-void record_trade(gint64 time, float price, float vol, const gchar *currency, const gchar *symbol) {
+void record_trade(gint64 time, float price, float vol) {
   GtkTreeIter iter;
   gtk_list_store_append(trade_store, &iter);
 
@@ -155,12 +182,41 @@ void record_trade(gint64 time, float price, float vol, const gchar *currency, co
                      COL_TIME, time,
                      COL_PRICE, price,
                      COL_VOL, vol,
-                     COL_CURR, currency,
-                     COL_SYM, symbol,
                      -1);
 }
 
+void update_ticker(gfloat buy, gfloat sell, gfloat low, gfloat high, gint64 volume) {
+  char *text;
+
+  text = asprintfx("<b>Buy:</b> %f", buy);
+  gtk_label_set_markup(ticker.buy, text);
+  free(text);
+
+  text = asprintfx("<b>Sell:</b> %f", sell);
+  gtk_label_set_markup(ticker.sell, text);
+  free(text);
+
+  text = asprintfx("<b>Low:</b> %f", low);
+  gtk_label_set_markup(ticker.low, text);
+  free(text);
+
+  text = asprintfx("<b>High:</b> %f", high);
+  gtk_label_set_markup(ticker.high, text);
+  free(text);
+
+  text = asprintfx("<b>Volume:</b> %d", volume);
+  gtk_label_set_markup(ticker.volume, text);
+  free(text);
+}
+
 gboolean tick(GIOChannel *, GIOCondition, gpointer);
+
+void json_reader_read_member_checked(JsonReader *reader, const gchar *member) {
+  if(!json_reader_read_member(reader, member)) {
+    const GError *e = json_reader_get_error(reader);
+    fprintf(stderr, "Error parsing data from server: %s\n", e->message);
+  }
+}
 
 void read_json(const gchar *buf, gsize len) {
   JsonParser *parser = json_parser_new();
@@ -174,30 +230,83 @@ void read_json(const gchar *buf, gsize len) {
   JsonReader *reader = json_reader_new(json_parser_get_root(parser));
 
   /* read the JSON tree */
-  json_reader_read_member(reader, "timestamp");
-  gint64 time = json_reader_get_int_value(reader);
-  json_reader_end_element(reader);
-  json_reader_read_member(reader, "price");
-  float price = strtof(json_reader_get_string_value(reader), NULL);
-  json_reader_end_element(reader);
-  json_reader_read_member(reader, "volume");
-  float vol = strtof(json_reader_get_string_value(reader), NULL);
-  json_reader_end_element(reader);
-  json_reader_read_member(reader, "currency");
-  gchar curr[64];
-  memcpy(curr, json_reader_get_string_value(reader),
-         64*sizeof(gchar));
-  json_reader_end_element(reader);
-  json_reader_read_member(reader, "symbol");
-  gchar sym[64];
-  memcpy(sym, json_reader_get_string_value(reader),
-         64*sizeof(gchar));
-  json_reader_end_element(reader);
+  json_reader_read_member(reader, "channel");
+  const char *key = json_reader_get_string_value(reader);
+  channel chan;
+  if(!strcmp(key, TRADE_CHANNEL)) {
+    chan = CHAN_TRADE;
+  } else if(!strcmp(key, TICKER_CHANNEL)) {
+    chan = CHAN_TICKER;
+  } else if(!strcmp(key, DEPTH_CHANNEL)) {
+    chan = CHAN_DEPTH;
+  } else {
+    chan = CHAN_ACCT;
+  }
+  json_reader_end_member(reader);
+
+  json_reader_read_member(reader, "op");
+  const char *op = json_reader_get_string_value(reader);
+  if(!op || !strcmp(op, "private")) {
+    json_reader_end_member(reader);
+    /* We should have data */
+    switch(chan) {
+    case CHAN_TRADE: {
+      json_reader_read_member(reader, "trade");
+
+      json_reader_read_member(reader, "amount");
+      gfloat amt = json_reader_get_double_value(reader);
+      json_reader_end_member(reader);
+
+      json_reader_read_member(reader, "date");
+      gint64 date = json_reader_get_int_value(reader);
+      json_reader_end_member(reader);
+
+      json_reader_read_member(reader, "price");
+      gfloat price = json_reader_get_double_value(reader);
+      json_reader_end_member(reader);
+
+      json_reader_end_member(reader);
+
+      record_trade(date, price, amt);
+      break;
+    }
+    case CHAN_TICKER: {
+      json_reader_read_member(reader, "ticker");
+
+      json_reader_read_member(reader, "buy");
+      gfloat buy = json_reader_get_double_value(reader);
+      json_reader_end_member(reader);
+
+      json_reader_read_member(reader, "high");
+      gfloat high = json_reader_get_double_value(reader);
+      json_reader_end_member(reader);
+
+      json_reader_read_member(reader, "low");
+      gfloat low = json_reader_get_double_value(reader);
+      json_reader_end_member(reader);
+
+      json_reader_read_member(reader, "sell");
+      gfloat sell = json_reader_get_double_value(reader);
+      json_reader_end_member(reader);
+
+      json_reader_read_member(reader, "vol");
+      gint64 vol = json_reader_get_int_value(reader);
+      json_reader_end_member(reader);
+
+      json_reader_end_member(reader);
+
+      update_ticker(buy, sell, low, high, vol);
+    }
+    case CHAN_DEPTH:
+    case CHAN_ACCT:
+      break;
+    }
+  }
+  json_reader_end_member(reader);
 
   g_object_unref(reader);
   g_object_unref(parser);
 
-  record_trade(time, price, vol, curr, sym);
 }
 
 gboolean do_connect(struct addrinfo *i) {
@@ -223,12 +332,10 @@ gboolean do_connect(struct addrinfo *i) {
       if(errno == EINPROGRESS) {
         /* Possible connection */
         current = p;
-        if(ticker) {
-          g_object_unref(ticker);
-        }
-        ticker = g_io_channel_unix_new(fd);
-        g_io_channel_set_flags(ticker, G_IO_FLAG_NONBLOCK, NULL);
-        g_io_add_watch(ticker, G_IO_OUT, tick, (gpointer)connctx);
+        stream = g_io_channel_unix_new(fd);
+        g_io_channel_set_encoding(stream, NULL, NULL);
+        g_io_channel_set_flags(stream, G_IO_FLAG_NONBLOCK, NULL);
+        g_io_add_watch(stream, G_IO_OUT, tick, (gpointer)connctx);
         break;
       } else {
         close(fd);
@@ -236,14 +343,11 @@ gboolean do_connect(struct addrinfo *i) {
         continue;
       }
     } else {
-      gtk_statusbar_push(statusbar, gtk_statusbar_get_context_id(statusbar, connctx), "Connected!");
-      if(ticker) {
-        g_object_unref(ticker);
-      }
-      ticker = g_io_channel_unix_new(fd);
-      g_io_channel_set_flags(ticker, G_IO_FLAG_NONBLOCK, NULL);
-      g_io_add_watch(ticker, G_IO_IN, tick, (gpointer)connctx);
-      freeaddrinfo(servinfo); // all done with this structure
+      gtk_statusbar_push(statusbar, gtk_statusbar_get_context_id(statusbar, connctx), "Handshaking...");
+      stream = g_io_channel_unix_new(fd);
+      g_io_channel_set_encoding(stream, NULL, NULL);
+      g_io_channel_set_flags(stream, G_IO_FLAG_NONBLOCK, NULL);
+      g_io_add_watch(stream, G_IO_OUT, tick, (gpointer)connctx);
     }
 
     break;
@@ -263,8 +367,58 @@ gboolean do_connect(struct addrinfo *i) {
   return TRUE;
 }
 
+gboolean handshake(GIOChannel *ch) {
+  static gsize total_written = 0;
+  const gsize len = strlen(WEBSOCKET_HELLO);
+  gsize new_written;
+  GError *e = NULL;
+
+  if(total_written == len) {
+    /* We already returned true, so this must be a reconnect */
+    total_written = 0;
+  }
+  
+  GIOStatus s = g_io_channel_write_chars(ch, WEBSOCKET_HELLO+total_written, len-total_written, &new_written, &e);
+  
+  if(s != G_IO_STATUS_NORMAL && s != G_IO_STATUS_AGAIN) {
+    GtkWidget *dialog = gtk_message_dialog_new(
+      NULL,
+      GTK_DIALOG_DESTROY_WITH_PARENT,
+      GTK_MESSAGE_ERROR,
+      GTK_BUTTONS_CLOSE,
+      "Failed to handshake with market data server:\n%s",
+      e->message);
+    gtk_dialog_run(GTK_DIALOG(dialog));
+    gtk_widget_destroy(dialog);
+    gtk_main_quit();
+  }
+
+  e = NULL;
+  s = g_io_channel_flush(ch, &e);
+
+  if(s != G_IO_STATUS_NORMAL && s != G_IO_STATUS_AGAIN) {
+    GtkWidget *dialog = gtk_message_dialog_new(
+      NULL,
+      GTK_DIALOG_DESTROY_WITH_PARENT,
+      GTK_MESSAGE_ERROR,
+      GTK_BUTTONS_CLOSE,
+      "Failed to handshake with market data server:\n%s",
+      e->message);
+    gtk_dialog_run(GTK_DIALOG(dialog));
+    gtk_widget_destroy(dialog);
+    gtk_main_quit();
+  }
+  
+  total_written += new_written;
+  
+  if(total_written == len) {
+    return TRUE;
+  }
+  return FALSE;
+}
+
 int try_connect() {
-  int res = resolve("bitcoincharts.com", "27007", &servinfo);
+  int res = resolve(WEBSOCKET_HOST, "80", &servinfo);
   if(res != 0) {
     GtkWidget *dialog = gtk_message_dialog_new(
       NULL,
@@ -287,49 +441,75 @@ int try_connect() {
 
 gboolean tick(GIOChannel *source, GIOCondition condition, gpointer data) {
   if(condition == G_IO_OUT) {
-    gtk_statusbar_push(statusbar, gtk_statusbar_get_context_id(statusbar, (const char*)data), "Connected!");
-    g_io_add_watch(ticker, G_IO_IN, tick, NULL);
-    freeaddrinfo(servinfo); // all done with this structure
-    /* Remove watch */
-    return FALSE;
+    gtk_statusbar_push(statusbar, gtk_statusbar_get_context_id(statusbar, (const char*)data), "Handshaking...");
+    if(servinfo) {
+      freeaddrinfo(servinfo); // all done with this structure
+      servinfo = NULL;
+    }
+    if(handshake(source)) {
+      g_io_add_watch(stream, G_IO_IN, tick, data);
+      gtk_statusbar_pop(statusbar, gtk_statusbar_get_context_id(statusbar, (const char*)data));
+      gtk_statusbar_push(statusbar, gtk_statusbar_get_context_id(statusbar, (const char*)data), "Connected!");
+      /* Remove watch */
+      return FALSE;
+    }
+    return TRUE;
   }
 
   GError *e = NULL;
   guint bytes;
-  GIOStatus s = g_io_channel_read_chars(ticker,
+  GIOStatus s = g_io_channel_read_chars(stream,
                                         readbuf+readbuf_end,
                                         readbuf_size-readbuf_end,
                                         &bytes, &e);
   if(s != G_IO_STATUS_NORMAL) {
-    g_io_channel_shutdown(ticker, FALSE, &e);
-    g_object_unref(ticker);
-    ticker = NULL;
+    gtk_statusbar_pop(statusbar, gtk_statusbar_get_context_id(statusbar, (const char*)data));
 
+    GtkWidget *dialog = gtk_message_dialog_new(
+      NULL,
+      GTK_DIALOG_DESTROY_WITH_PARENT,
+      GTK_MESSAGE_ERROR,
+      GTK_BUTTONS_CLOSE,
+      "Lost connection to market data source:\n%s\nAttempting to reconnect.",
+      e->message);
+    gtk_dialog_run(GTK_DIALOG(dialog));
+    gtk_widget_destroy(dialog);
+    
+    e = NULL;
+
+    g_io_channel_shutdown(stream, FALSE, &e);
+    stream = NULL;
+    
     if(try_connect()) {
       gtk_main_quit();
     }
-    gtk_statusbar_pop(statusbar, gtk_statusbar_get_context_id(statusbar, (const char*)data));
-    
   }
-  ssize_t i;
-  ssize_t last = 0;
-  const ssize_t total = readbuf_end + bytes;
+  
+  gsize i;
+  static gssize begin = -1;
+  const gsize total = readbuf_end + bytes;
   for(i = readbuf_end; i < total; ++i) {
-    if(readbuf[i] == '\0' && i-last > 0) {
-      read_json(readbuf+last, i-last);
-      last = i+1;
+    if(begin < 0) {
+      if(readbuf[i] == (gchar)0x00) {
+        begin = i + 1;
+      }
+    } else {
+      if(readbuf[i] == (gchar)0xFF) {
+        read_json(readbuf+begin, i-begin);
+        begin = -1;
+      }
     }
   }
 
-  if(total-last > 0) {
-    if(last == 0 && total == readbuf_size) {
+  if(begin < 0) {
+    readbuf_end = 0;
+  } else {
+    if(begin == 0 && total == readbuf_size) {
       readbuf_size *= 2;
       readbuf = realloc(readbuf, readbuf_size*sizeof(gchar));
     }
-    readbuf_end = total-last;
-    memmove(readbuf, readbuf+last, readbuf_end*sizeof(gchar));
-  } else {
-    readbuf_end = 0;
+    readbuf_end = total-begin;
+    memmove(readbuf, readbuf+begin, (readbuf_end-begin)*sizeof(gchar));
   }
 
   return TRUE;
@@ -354,7 +534,7 @@ int main(int argc, char **argv) {
   if(argc == 2 && !strcmp("test", argv[1])) {
     unsigned i;
     for(i = 0; i < 100; ++i) {
-      record_trade(i, i*0.1, i*1.4, "USD", "FAKE");
+      record_trade(i, i*0.1, i*1.4);
     }
   }
 
